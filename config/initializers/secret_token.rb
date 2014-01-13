c9f3ce7d98cf670b40449a00608b0ac6ec76d679
jarra <suhrawardi@gmail.com>
2014-01-13 20:34:01 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bank::Application.config.secret_key_base = '89ccd67ce966be28b713c8cc796fc41e64f7123b04aa4c1eddf839ff220e004906d812a9c123253e67d9da8b494233d93b422b66e86fc3b96822c17c0f3d1b2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bank::Application.config.session_store :cookie_store, key: '_bank_session'
